3305ef08a9a0c48e37cdef6fc55b2cb0154d9cea
Debora-Mind <debora.almeida.de.mello@gmail.com>
2024-11-20 17:20:32 -0300

    Adição de fontes
    Fontes adicionadas e aplicadas ao tema do APP

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -9,18 +9,22 @@ class CategoryItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              category.color.withOpacity(0.6),
-              category.color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: BorderRadius.circular(15),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.6),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
-        child: Text(category.title));
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleSmall,
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Vamos Cozinhar?',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        hintColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodySmall: const TextStyle(
+                color: Color.fromARGB(255, 61, 61, 61),
+              ),
+              bodyMedium: const TextStyle(
+                color: Colors.black,
+              ),
+              bodyLarge: const TextStyle(
+                color: Colors.black,
+              ),
+              titleSmall: const TextStyle(
+                fontSize: 18,
+                fontFamily: 'RobotoCondensed',
+                fontStyle: FontStyle.italic,
+              ),
+              titleMedium: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.normal,
+              ),
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.pink,
           titleTextStyle: TextStyle(
             color: Colors.white,
             fontSize: 24,
-            fontWeight: FontWeight.bold,
+            fontFamily: 'Realway',
+            fontWeight: FontWeight.w300,
           ),
           centerTitle: true,
           shape: Border(
             bottom: BorderSide(
-              color: Colors.lightBlue,
+              color: Colors.pinkAccent,
               width: 2,
             ),
           ),
@@ -35,6 +65,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -44,9 +75,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Vamos Cozinhar?'),
+        title: const Text('Vamos Cozinhar?'),
       ),
-      body: Center(
+      body: const Center(
         child: Text('Navegar é preciso!!'),
       ),
     );
