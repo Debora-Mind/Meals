f81164679c5eb80a162ccbdaaba32dbd080b424d
Debora-Mind <debora.almeida.de.mello@gmail.com>
2024-11-20 21:38:55 -0300

    Criada rota para página não encontrada

---
+++ lib/screens/unknown_route_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:meals/utils/app_routes.dart';
+
+class UnknownRouteScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ops...'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.error_outline,
+              size: 100,
+              color: Colors.red,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Ops! Página não encontrada!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54,
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Parece que você seguiu um link inválido ou a página foi removida.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(
+                  context,
+                  AppRoutes.HOME,
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+              ),
+              child: const Text(
+                'Voltar para a página inicial',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
